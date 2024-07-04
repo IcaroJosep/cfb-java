abdcec155abadf77855931460f1a0b53a52f1e9d
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-07-04 20:45:34 -0300

    *

--- 009-metodos/src/Principal.java
+++ 009-metodos/src/Principal.java
@@ -1,9 +1,26 @@
 
 public class Principal {
-
+/* METODOS:
+ * 		
+ * 		"METODOS SAO COISAS Q AS CLKASES FAZEM !"
+ * 
+ * -Senpre se endica o modificador de acesso ex:(public);
+ * -A classe principal(MAIN), que contém o main, geralmente deve ter como objetivo principal iniciar e orquestrar a execução do programa.
+ *Incluir métodos não estáticos (STATIC) com outras funcionalidades desvia o foco dessa responsabilidade principal, tornando o código menos coeso e organizado,
+ *E também a para chamar um metodo nao estatico gera a nessecidade da instanciacao de um obj *
+ * -para a criacao do metodo tbm é necessário declarar o tipo de retorno .
+ * 
+ *-Sobrecarga de métodos (method overloading)
+ *   METODOS tbm podem ussar o mesmo nome des de q seus parametros sejao diferentes ou tipo de retorno
+ * 	    Parâmetros:
+ *			O número de parâmetros pode ser diferente.
+ *			O tipo dos parâmetros pode ser diferente.
+ *			A ordem dos parâmetros pode ser diferente.
+ *		Tipo de retorno:
+ *			O tipo de retorno pode ser diferente.
+ */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- 
+		
 	}
 
 }
