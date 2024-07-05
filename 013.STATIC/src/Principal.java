35ef46e645a0e3a8dc88bf0e7b7d5ef79350e95b
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-07-05 20:54:04 -0300

    ,

--- 013.STATIC/src/Principal.java
+++ 013.STATIC/src/Principal.java
@@ -1,10 +1,24 @@
 
 public class Principal {
-/*	
+/*	quando se define um elemanto como estatico(static) 
+ * ele é criado em uma posicao da memoria
+ * e para todos os OBJ da classe ele estara na 
+ * mesma posicao da memoria .
+ * 
+ * ou seja se eu alterar uma variavel statica todos 
+ * os objs da clase terao o valor alterado
+ * 
+ * metodos e atributos static's:
+ * nao precisao da instanciaçao da classe para serem chamados
+ * 
  * 
  * */
 	public static void main(String[] args) {
 	
+		
+		
+		
+		
 	}
 
 }
