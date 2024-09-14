6d93cc491b4af9dafef7b671160c6922a7ce50b1
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-09-14 20:13:12 -0300

    .

---
+++ 029.funcao com numero indeterminado de argumentos/src/Main.java
@@ -0,0 +1,18 @@
+
+
+public abstract class Main {
+
+	public static void main(String[] args) {
+	String a ="951911";
+	int b=Integer.parseInt(a);
+	System.out.printf("%d",b);
+	
+	
+		
+	}
+	/*public static Integer soma() {
+		Integer res=0;
+		
+		return res;
+	}*/
+}
