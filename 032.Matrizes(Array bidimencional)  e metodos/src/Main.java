00ecbd7514e029a93eb807f8262b59b7a816a821
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-09-15 13:53:24 -0300

    Update Main.java

--- 032.Matrizes(Array bidimencional)  e metodos/src/Main.java
+++ 032.Matrizes(Array bidimencional)  e metodos/src/Main.java
@@ -1,9 +1,30 @@
+import java.security.SecureRandom;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final int  linha=3;
+		final int coluna=5;
+				
+		int [][] numeros = new int [linha][coluna];
+		
+		
+		for(int l=0;l<linha;l++) {
+			for(int c=0;c<coluna;c++) {
+				numeros[l][c]= new SecureRandom().nextInt(100);
+			}
+		}
+		
+		
+		
+		
+		for(int[] l:numeros) {
+			for(int c:l) {
+				System.out.print(c + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
