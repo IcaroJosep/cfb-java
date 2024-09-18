0ad21763514d5ce36cc3705e6fea509dddea8b3d
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-09-18 14:38:19 -0300

    Strings

--- 38.THROW, lançando uma exceção/src/Main.java
+++ 38.THROW, lançando uma exceção/src/Main.java
@@ -1,25 +1,44 @@
 
-
 public class Main {
-
+/*
+ * String é uma classe portanto possue metodos 
+ *  
+ * String é uma classe q comtem um array de caracteres 
+ * */
 	public static void main(String[] args) {
-		
-		int nota1,nota2,res;
-		nota1=30;
-		nota2=60;
 	
-		//gera uma execao com msg pesonalisada
-//throw new IllegalArgumentException("erro");						
+	char[] texto_c= {'c','u','r','s','o',' ','j','a','v','a'};	
+	String texto_s=new String(texto_c);
+	
+	System.out.println(texto_s);
+	
+//tbm é possivel selecionar as pozicoes q se deseja pegar
+	String c =new String(texto_c,0,5);
+	System.out.println(c);
+	
+//metodos 
 		
-		
-		if (nota1>50||nota2>50) {
-			throw new IllegalArgumentException("erro");						
-		}else {
-			res=(nota1+nota2);	
-			System.out.print(res);
-		}
-		
-			
+		//length retorna o tamanho da string
+	
+	int tam=texto_s.length();
+	System.out.println(tam);
+	
+	
+		//CharAt retorna o caracter na posicao
+	char caracter=texto_s.charAt(2);
+	System.out.println(caracter);
+	
+	
+	    //getChars copia caracteres de uma string para outra
+	//char[] gat =new char[10];
+	char[] gat = {'*','*','*','*','*','*','*','*','*','*'};
+	texto_s.getChars(6, 10, gat, 1);
+	System.out.println(gat);
+	
+	
+	
+	
+	
 	}
 
 }
