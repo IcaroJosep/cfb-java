0d938fcde925b3c2fbf3a3f4cfc2ea23278275cb
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-09-18 13:57:20 -0300

    .

--- 38.THROW, lançando uma exceção/src/Main.java
+++ 38.THROW, lançando uma exceção/src/Main.java
@@ -7,9 +7,10 @@ public class Main {
 		int nota1,nota2,res;
 		nota1=30;
 		nota2=60;
-		
+	
+		//gera uma execao com msg pesonalisada
 //throw new IllegalArgumentException("erro");						
-
+		
 		
 		if (nota1>50||nota2>50) {
 			throw new IllegalArgumentException("erro");						
