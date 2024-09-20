c4455a53c91d23bf55f4d84911259f520030ed84
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-09-19 21:06:28 -0300

    perca de class na parte 39

---
+++ 39.Strings  metodos/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+			/// esta clase foi perdida !!!!
+		/*porem ela mostrav metodos basicos
+		 * 
+		 * como modos diferemtes de de costritor
+		 * ex: pasar array de char diretamente para costrucao de strings
+		 * 
+		 * e metodos como :
+		 * .length() // pega tamanho
+		 * .charAt() captura char no index indicado
+		 * .getChars()copia para outro 
+		 * */
+		
+		/*colocarei uma foto com os metodos na pasta*/
+		
+				
+						
+	}
+
+}
