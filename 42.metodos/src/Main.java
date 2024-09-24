55223b9af25c3419f422a236bd2dd9c27f55dc18
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-09-23 23:11:43 -0300

    fim strings

---
+++ 42.metodos/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("REPLACE================================");
+// replace		
+		/*ela subistitui determinada parte da string*/
+		String s1="Icaro josep";
+		//String s2;
+		//s2=s1.replace("osep", " r machado");	
+		//System.out.println(s2);	
+		
+		/*pode ser usado diretamente no printf sem a adicao de nova string*/
+		System.out.println(s1.replace("osep", " r machado"));	
+		
+		
+		System.out.println("\n\ntoLowerCase=============================");
+//toLowerCase
+		/*comverte toda a string para minusculo*/
+		String s3="ICARO";
+		System.out.println(s3.toLowerCase());
+		
+		System.out.println("\n\toUpperCase=============================");
+//toUpperCase
+		/*comverte toda a string para maiusculo*/
+		String s4="icaro";
+		System.out.println(s3.toUpperCase());
+
+//split
+		/*define um elemento como quebra como linha*/
+		
+
+	}
+
+}
