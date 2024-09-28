bc84f08ce175c79289f9186747cc0326bd623580
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-09-27 22:26:51 -0300

    .

--- 51.2 interfaces/src/Main.java
+++ 51.2 interfaces/src/Main.java
@@ -1,9 +1,28 @@
-
 public class Main {
-
+/* a lib math e uma biblioteca com algumas funoes matematica
+ * 
+ * */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//.min / retorna o menor entre 2
+		int v1= Math.min(6, 5);
+		System.out.println(v1);
+		
+		//.sqrt /retorna a rais quadrada
+		double v2 = Math.sqrt(156);
+		System.out.println(v2);
+	
+		//.abs /retorna valor absoluto
+		double v3 =-5.16;
+		System.out.println(Math.abs(v3));
+		
+		//.random // retorna umero aleatorio entre 0 e 1
+		double v4 = Math.random()/**11*/;
+		System.out.println(v4);
+		
+		//floor // reorna o inteiro mais proximo
+		System.out.println(Math.floor(v4*11));
+		
+	
 	}
 
 }
